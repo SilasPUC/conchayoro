703388bf82692b9c47f6f67a030ffdcd62bac9ec
SilasPUC <santos.silas@outlook.com>
2024-09-21 14:16:19 -0300

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono
 text-sm lg:flex">
-        <h1> Welcome to the ConchaYOro App </h1>
+        <h1> Welcome to the ConchaYOro App - deploy v2 </h1>
       </div>
     </main>
   )
